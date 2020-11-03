d7321288c318422e3280942f4292ad794fbc15e8
wenzh3333 <490416772@qq.com>
2020-11-03 14:48:01 +0800

    删除了Hallo

--- app/src/main/java/com/wenzh/mycalculator/MainActivity.java
+++ app/src/main/java/com/wenzh/mycalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.wenzh.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.e("123","321");
     }
 }
